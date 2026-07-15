7fc0e262e87bd2cc1992c2a0a14a964c482c2f0a
Kaiwen Yao <kaiwen.yao.cn@outlook.com>
2026-07-15 13:10:09 +0100

    fix: resolve server.js scheduler import conflict

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const { installConsoleTimestamps } = require('./src/utils/consoleTimestamps');
 installConsoleTimestamps();
 
 const app = require('./src/app');
-const { startCrowdPredictionScheduler } = require('./src/jobs/scheduler');
+const {
+    startCrowdPredictionScheduler,
+    startHeatmapPredictionScheduler
+} = require('./src/jobs/scheduler');
 const { startH3GridScoreRefreshScheduler } = require('./src/jobs/h3GridScoreScheduler');
 
 const PORT = process.env.PORT || 3000;
@@ -13,5 +16,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Zentra Backend Server running on http://localhost:${PORT}`);
     startCrowdPredictionScheduler();
+    startHeatmapPredictionScheduler();
     startH3GridScoreRefreshScheduler();
 });
